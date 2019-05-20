e972fe377871f8ffc6f362679ead2a1ff03753ee
jida <jakazzy@gmail.com>
2019-05-20 09:03:21 +0000

    retrieve data from state and display

--- src/Note.js
+++ src/Note.js
@@ -8,27 +8,41 @@ class Note extends Component{
       {title: "sed est blanditiis ea, veniam commodi eligendi eum repellendus.", id: 3}]
   }
     render(){
-      return ( <div className="row">
-        <div className="col s12  m8 offset-m2">
-          <div className="card medium">
-              <nav className="red darken-1">
-                <div className="nav-wrapper">
-                    <div className="card-title center">Card Title</div>
-                </div>
-              </nav>
-              <div className="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-              </div>
-              <div className="card-action">
-                <span className="left">Mon 20 May, 2019 at 12:42AM</span>
-                <div className="right">
-                    <span className="btn-floating btn-medium waves-effect waves-light red"><i className="material-icons">add</i></span>
-                    <span className="btn-floating btn-medium waves-effect waves-light red"><i className="material-icons">delete</i></span>
-                    <span className="btn-floating btn-medium waves-effect waves-light red"><i className="material-icons">edit</i></span>
-                </div>
-              </div>
-            </div>
-          </div>
+      const { notes } = this.state;
+      const notesList = notes.length ? ( 
+        notes.map((note) => {
+          return( <div key={ note.id } className="row" >
+                  <div className="col s12  m8 offset-m2">
+                  <div className="card medium">
+                    <nav className="red darken-1">
+                      <div className="nav-wrapper">
+                        <div className="card-title center">Card Title</div>
+                      </div>
+                    </nav>
+                    <div className="card-content">
+                      <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div className="card-action">
+                      <span className="left">Mon 20 May, 2019 at 12:42AM</span>
+                      <div className="right">
+                          <span className="btn-floating btn-medium waves-effect waves-light red"><i className="material-icons">add</i></span>
+                          <span className="btn-floating btn-medium waves-effect waves-light red"><i className="material-icons">delete</i></span>
+                          <span className="btn-floating btn-medium waves-effect waves-light red"><i className="material-icons">edit</i></span>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+
+                  </div>
+                  )
+        })
+      ):(<div className="center">No notes made yet</div> )
+      return ( 
+      <div >
+        
+              { notesList }
+              
+            
         </div>
 
       )
