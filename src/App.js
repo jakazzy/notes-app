1f5fece3f2b023e5f1055bd8df66bca0e4ed1743
jida <jakazzy@gmail.com>
2019-05-20 01:48:39 +0000

    add note component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Note from "./Note";
 
 
-function App() {
-  return (
-    <div className="App">
-      I am an application
-    </div>
-  );
+class App extends Component{
+  render(){
+    return ( <div className = "App">
+        <Note/>
+        </div>
+    )
+  }
+    
 }
 
 export default App;
