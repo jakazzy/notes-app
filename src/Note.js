07ad7fa6a350720a36718cb8615f9fd001ff2b75
jida <jakazzy@gmail.com>
2019-05-20 09:20:47 +0000

    modify retrieve data

--- src/Note.js
+++ src/Note.js
@@ -16,11 +16,11 @@ class Note extends Component{
                   <div className="card medium">
                     <nav className="red darken-1">
                       <div className="nav-wrapper">
-                        <div className="card-title center">Card Title</div>
+                        <div className="card-title center">{ note.title }</div>
                       </div>
                     </nav>
                     <div className="card-content">
-                      <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                      <p>{  note.body}</p>
                     </div>
                     <div className="card-action">
                       <span className="left">Mon 20 May, 2019 at 12:42AM</span>
