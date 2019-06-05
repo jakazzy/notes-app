ca3e711754589c18ee03322ec72b89ce28c08aa3
jida <jakazzy@gmail.com>
2019-06-05 08:55:42 +0000

    edit add new note

--- src/Note.js
+++ src/Note.js
@@ -23,38 +23,37 @@ class Note extends Component{
   }
   
     render(){
-      
-          return( <div className="row" >
-                  <div className="col s12  m8 offset-m2">
-                  <div className="card medium">
-                    <nav className="red darken-1">
-                      <div className="nav-wrapper">
-                        <div tabIndex="0" className="card-title center">
-                        <p><span className="left date">Mon 20 May, 2019 at 12:42AM</span></p>
-                        </div>
-                      </div>
-                    </nav>
-                    <div className="card-content">
-                      <ContentEditable
-                        html = { this.state.content}
-                        disabled ={ false }
-                        onChange ={ this.handlechange }
-                        className = "content"
-                        onBlur ={this.handleSave}
-                      />
-                    </div>
-                    <div className="card-action">
-                      
-                      <div className="right">
-                          <span onClick ={ () => {this.props.deleteNote(this.props.note.id)}} className="btn-floating btn-medium waves-effect waves-light red"><i className="material-icons">delete</i></span>
-                          <span className="btn-floating btn-medium waves-effect waves-light red"><i className="material-icons">edit</i></span>
-                      </div>
-                    </div>
+    return( <div className="row" >
+            <div className="col s12  m8 offset-m2">
+            <div className="card medium">
+              <nav className="red darken-1">
+                <div className="nav-wrapper">
+                  <div tabIndex="0" className="card-title center">
+                  <p><span className="left date">Mon 20 May, 2019 at 12:42AM</span></p>
                   </div>
-                  </div>
+                </div>
+              </nav>
+              <div className="card-content">
+                <ContentEditable
+                  html = { this.state.content}
+                  disabled ={ false }
+                  onChange ={ this.handlechange }
+                  className = "content"
+                  onBlur ={this.handleSave}
+                />
+              </div>
+              <div className="card-action">
+                
+                <div className="right">
+                    <span onClick ={ () => {this.props.deleteNote(this.props.note.id)}} className="btn-floating btn-medium waves-effect waves-light red"><i className="material-icons">delete</i></span>
+                    <span className="btn-floating btn-medium waves-effect waves-light red"><i className="material-icons">edit</i></span>
+                </div>
+              </div>
+            </div>
+            </div>
 
-                  </div>
-                  )
+            </div>
+            )
     }
       
   }
